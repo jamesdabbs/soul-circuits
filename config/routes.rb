c6867589f6bf2db4eb343fa380c18171f4405a46
James Dabbs <jamesdabbs@gmail.com>
2014-09-06 13:28:21 -0400

    Scope down playlist actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
 
-  resources :playlists do
+  resources :playlists, only: [:new, :create, :show, :index] do
     post :like
 
     # TODO: listen?
